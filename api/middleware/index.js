0cd3cf99781d900b6472c996cd84963bc8a918d8
tomas0011 <tomasbenjamin117@gmail.com>
2022-10-18 21:00:48 -0300

    change cache name

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,14 +1,14 @@
 const getExpeditiousCache = require('express-expeditious');
 
 const defaultOptions = {
-    namespace: 'expresscache',
+    namespace: 'epersistencias_cache',
     defaultTtl: '1 minute',
     statusCodeExpires: {
         404: '5 minutes',
         500: 0
     }
-}
+};
 
-const cacheInit = getExpeditiousCache(defaultOptions)
+const cacheInit = getExpeditiousCache(defaultOptions);
 
-module.exports = { cacheInit }
+module.exports = { cacheInit };
