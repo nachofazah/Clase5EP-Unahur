3217d882f16ac32a1edebafde66906224142e965
tomas0011 <tomasbenjamin117@gmail.com>
2022-11-09 02:36:23 -0300

    add nodemon and fix validateTokenMiddleware bug

--- api/middlewares/validateTokenMiddleware.js
+++ api/middlewares/validateTokenMiddleware.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 
-const validateTokenMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
-  const request = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${process.env.FIREBASE_API_KEY}`;
-  axios.post(request, { idToken: token.substr(7) })
-    .then(next())
-    .catch(() => {
-      res.status(403).send({ message: 'invalid token' })
-    })
+const validateTokenMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const request = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${process.env.FIREBASE_API_KEY}`;
+    await axios.post(request, { idToken: token.substr(7) });
+    next();
+  } catch (error) {
+    res.status(403).send({ message: 'invalid token' });
+  }
 }
 
 module.exports = validateTokenMiddleware;
